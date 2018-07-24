e33369c57f16a68eba977fe6f3ad1f21fed88f96
Olaf Szmidt <szmidtolaf@gmail.com>
2018-07-24 15:13:57 +0100

    Remove django reversion

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(name='codeforlife-portal',
           'django-online-status==0.1.0',
 
           'Pillow==3.3.2',
-          'django-reversion==1.9.3',
           'sqlparse',
           'libsass',
           'django-forms-bootstrap'
